af1f3efe9922800e2854967d7cc2b8a1e76318fb
ExaAlice <48439969+ExaAlice@users.noreply.github.com>
2023-02-20 02:08:56 +0800

    Update

--- Script/Network_delay.js
+++ Script/Network_delay.js
@@ -1,6 +1,15 @@
 //使用方法
-******************************************
+//******************************************
+#!name=Connectivity_Test
+#!desc=网络连通性测试 Baidu,Bilibili,Google,YouTube,Github
 
+[Script]
+Connectivity_Test = type=generic,timeout=3,script-path=https://raw.githubusercontent.com/Keywos/rule/main/JS/ConnectivityTest.js,argument=title=Connectivity Test&server=false
+
+[Panel]
+Connectivity_Test = script-name=Connectivity_Test,update-interval=1
+//
+******************************************
 const REQUEST_HEADERS = {
   'User-Agent':
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
