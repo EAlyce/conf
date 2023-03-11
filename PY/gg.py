98ef6501870efffe0d56e52d0cec5a7df631890b
ExaAlice <48439969+ExaAlice@users.noreply.github.com>
2023-03-11 17:49:32 +0800

    Update gg.py

--- PY/gg.py
+++ PY/gg.py
@@ -2,15 +2,10 @@ from pagermaid import Config, log
 from pagermaid.listener import listener
 from pagermaid.enums import Message
 from pagermaid.utils import lang, pip_install
+pip_install("googlesearch-python")
+from googlesearch import search
 
-pip_install("google-api-python-client")
-
-from googleapiclient.discovery import build
-
-
-@listener(command="google",
-          description=lang('google_des'),
-          parameters="[query]")
+@listener(command="google", description=lang('google_des'), parameters="[query]")
 async def google(message: Message):
     """ Searches Google for a string. """
     query = message.arguments
@@ -18,15 +13,14 @@ async def google(message: Message):
         if not message.reply_to_message:
             return await message.edit(lang('arg_error'))
         query = message.reply_to_message.text
-    service = build("customsearch", "v1", developerKey="AIzaSyCkFJGlkkvPRkNpq5A1ioFTjgqwgFFrOFU")
     query = query.replace(' ', '+')
     if not Config.SILENT:
         message = await message.edit(lang('google_processing'))
     results = ""
-    for i in service.cse().list(q=query, cx="YOUR_SEARCH_ENGINE_ID").execute()['items']:
+    for i in search(query, num_results=5):
         try:
-            title = i['title'][:30] + '...'
-            link = i['link']
+            title = i.title[:30] + '...'
+            link = i.url
             results += f"\n<a href=\"{link}\">{title}</a> \n"
         except Exception:
             return await message.edit(lang('google_connection_error'))
