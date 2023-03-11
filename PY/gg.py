b5a6b663090766e8f8c7b1e55b492cadf212f23d
ExaAlice <48439969+ExaAlice@users.noreply.github.com>
2023-03-11 17:24:13 +0800

    Create gg.py

---
+++ PY/gg.py
@@ -0,0 +1,34 @@
+from pagermaid import Config, log
+from pagermaid.listener import listener
+from pagermaid.enums import Message
+from pagermaid.utils import lang, pip_install
+
+pip_install("google-api-python-client")
+
+from googleapiclient.discovery import build
+
+
+@listener(command="google",
+          description=lang('google_des'),
+          parameters="[query]")
+async def google(message: Message):
+    """ Searches Google for a string. """
+    query = message.arguments
+    if not query:
+        if not message.reply_to_message:
+            return await message.edit(lang('arg_error'))
+        query = message.reply_to_message.text
+    service = build("customsearch", "v1", developerKey="AIzaSyCkFJGlkkvPRkNpq5A1ioFTjgqwgFFrOFU")
+    query = query.replace(' ', '+')
+    if not Config.SILENT:
+        message = await message.edit(lang('google_processing'))
+    results = ""
+    for i in service.cse().list(q=query, cx="YOUR_SEARCH_ENGINE_ID").execute()['items']:
+        try:
+            title = i['title'][:30] + '...'
+            link = i['link']
+            results += f"\n<a href=\"{link}\">{title}</a> \n"
+        except Exception:
+            return await message.edit(lang('google_connection_error'))
+    await message.edit(f"<b>Google</b> |<code>{query}</code>| 🎙 🔍 \n{results}", disable_web_page_preview=True)
+    await log(f"{lang('google_success')} `{query}`")
