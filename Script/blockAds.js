efb2d9b0364c8945243951663d7fd817c06352aa
ExaAlice <48439969+ExaAlice@users.noreply.github.com>
2023-10-11 19:20:52 +0800

    Update

--- Script/blockAds.js
+++ Script/blockAds.js
@@ -1,7 +1,7 @@
 let body = $response.body;
 
-// 使用正则表达式匹配并删除特定的HTML区块
-body = body.replace(/<div class="slide-baidu">[\s\S]*?<\/div>\s*<div class="mod mod-page" id="ChapterView"[\s\S]*?<span>/, '<span>');
+// 仅删除 <div class="slide-baidu">...</div> 广告区块
+body = body.replace(/<div class="slide-baidu">[\s\S]*?<\/div>/, '');
 
 // 使用$done()返回修改后的响应内容
 $done({body: body});
