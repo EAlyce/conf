e8349735f0f72875c0c8a4bef2404675aa26b3a5
Els <48439969+EAlyce@users.noreply.github.com>
2023-12-22 22:16:17 +0800

    Update RXsetup.sh

--- PagerMaid/RXsetup.sh
+++ PagerMaid/RXsetup.sh
@@ -1,6 +1,6 @@
 cd /var/lib
 sudo find /var/lib/ -type f -name "Pagermaid.sh*" -exec rm -f {} \;
-wget https://raw.githubusercontent.com/EAlyce/conf/main/PagerMaid/Pagermaid.sh
+curl -O https://raw.githubusercontent.com/EAlyce/conf/main/PagerMaid/Pagermaid.sh
 sudo mv /var/lib/PagerMaid-Pyro /var/lib/pagermaid
 sudo mkdir -p /var/lib/pagermaid/data
 chmod +x Pagermaid.sh
