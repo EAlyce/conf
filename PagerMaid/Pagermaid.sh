cb8d3b84f006528043131515cd3e4ca698aec41d
Els <48439969+EAlyce@users.noreply.github.com>
2023-12-22 23:10:28 +0800

    Update Pagermaid.sh

--- PagerMaid/Pagermaid.sh
+++ PagerMaid/Pagermaid.sh
@@ -28,32 +28,35 @@ check_sys() {
             release="debian"
         elif [[ $ID == "ubuntu" || $ID_LIKE == "ubuntu" ]]; then
             release="ubuntu"
+        else
+            echo "错误：本脚本只支持 Debian 和 Ubuntu。" 1>&2
+            exit 1
         fi
+    else
+        echo "错误：无法检测操作系统。" 1>&2
+        exit 1
     fi
 }
 
 install_python() {
-    
-    apt-get update -y >>/dev/null 2>&1
-    apt-get install -y python3 python3-pip neofetch libzbar-dev git >>/dev/null 2>&1
+    apt-get update -qq && apt-get upgrade -y -qq
+    apt-get install -y python3 python3-pip neofetch libzbar-dev git -qq
 }
 
 configure() {
-
     sudo mkdir -p /var/lib/pagermaid/data
-	cd /var/lib/pagermaid
+    cd /var/lib/pagermaid
     config_file=/var/lib/pagermaid/data/config.yml
     echo "生成配置文件中 . . ."
     cp /var/lib/pagermaid/config.gen.yml $config_file
-    printf "请输入应用程序 api_id："
-    read -r api_id <&1
+    read -p "请输入应用程序 api_id：" -e api_id
     sed -i "s/ID_HERE/$api_id/" $config_file
-    printf "请输入应用程序 api_hash："
-    read -r api_hash <&1
+    read -p "请输入应用程序 api_hash：" -e api_hash
     sed -i "s/HASH_HERE/$api_hash/" $config_file
 }
 
 login_screen() {
+    cd /var/lib/pagermaid
     python3 -m pagermaid
     systemctl_reload
 }
@@ -83,10 +86,11 @@ start_installation() {
     check_sys
     check_root
     check_ip
-
     echo "正在克隆仓库"
-    install_python
-    git clone https://github.com/TeamPGM/PagerMaid-Pyro.git
+    if ! git clone https://github.com/TeamPGM/PagerMaid-Pyro.git; then
+        echo "错误：无法克隆仓库。" 1>&2
+        exit 1
+    fi
     mv PagerMaid-Pyro /var/lib/pagermaid
     
     cd /var/lib/pagermaid
@@ -94,6 +98,12 @@ start_installation() {
     mkdir -p /var/lib/pagermaid/data
     configure
     login_screen
+    log_file="/var/lib/pagermaid/data/pagermaid.log.txt"
+    key1="PagerMaid-Pyro"
+    key2="已启动"
+    key3=",help"
+    key4="以获得帮助消息"
+    tail -f $log_file | awk "/$key1/ && /$key2/ && /$key3/ && /$key4/ {exit}" || exit 1
     echo "完成"
 }
 
@@ -102,14 +112,15 @@ cleanup() {
         echo "目录不存在不需要卸载。"
     else
         echo "正在关闭 PagerMaid . . ."
-		cd /
-        systemctl disable pagermaid >>/dev/null 2>&1
+        if ! systemctl disable pagermaid >>/dev/null 2>&1; then
+            echo "错误：无法关闭 PagerMaid。" 1>&2
+            exit 1
+        fi
         systemctl stop pagermaid >>/dev/null 2>&1
         echo "正在删除 PagerMaid 文件 . . ."
-		
+        
         rm -rf /etc/systemd/system/pagermaid.service >>/dev/null 2>&1
         rm -rf /var/lib/pagermaid >>/dev/null 2>&1
-		cd /
 
         echo "卸载完成 . . ."
     fi
