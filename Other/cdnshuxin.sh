73c165c916207348f30432a7f9991f5a524ceb9f
Els <48439969+EAlyce@users.noreply.github.com>
2023-12-24 17:45:42 +0800

    Update cdnshuxin.sh

--- Other/cdnshuxin.sh
+++ Other/cdnshuxin.sh
@@ -10,11 +10,21 @@ urls=(
   "https://purge.jsdelivr.net/gh/EAlyce/conf@main/Surge.conf"
 )
 
-# 循环访问链接
+# 循环访问链接并刷新
 for url in "${urls[@]}"; do
   # 使用 curl 访问链接，指定 HTTP 方法为 GET，添加 -H 模拟用户代理信息
-  result=$(curl -s -I -X GET -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" "$url" | head -n 1)
+  result=$(curl -s -X GET -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" "$url")
+
+  # 使用 jq 解析 JSON 数据中的 status 字段
+  status=$(echo "$result" | jq -r '.status')
 
   # 输出结果
   echo "访问 $url 的结果：$result"
+
+  # 判断 status 是否为 "finished"
+  if [[ "$status" == "finished" ]]; then
+    echo "刷新成功！"
+  else
+    echo "刷新未成功"
+  fi
 done
