34058a943be498031be43c71ded6ff4a4f411a04
Alas <48439969+EAlyce@users.noreply.github.com>
2024-01-06 04:30:48 +0800

    更新 RXsetup.sh

--- PagerMaid/RXsetup.sh
+++ PagerMaid/RXsetup.sh
@@ -42,6 +42,20 @@ source ~/.bashrc
 
 }
 
+
+
+# 定义设置 PATH 的函数
+set_custom_path() {
+    # 检查是否存在 PATH 变量，如果不存在则设置
+    PATH_CHECK=$(crontab -l | grep -q '^PATH=' && echo "true" || echo "false")
+
+    if [ "$PATH_CHECK" == "false" ]; then
+        # 设置全面的 PATH
+        PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+    fi
+}
+#!/bin/bash
+
 install_pagermaid() {
     local install_type="$1"
     local installer_url
@@ -76,61 +90,89 @@ install_pagermaid() {
     "./$(basename "$installer_url")"
 }
 
-# 定义设置 PATH 的函数
-set_custom_path() {
-    # 检查是否存在 PATH 变量，如果不存在则设置
-    PATH_CHECK=$(crontab -l | grep -q '^PATH=' && echo "true" || echo "false")
+#!/bin/bash
 
-    if [ "$PATH_CHECK" == "false" ]; then
-        # 设置全面的 PATH
-        PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+install_pagermaid() {
+    local install_type="$1"
+    local installer_url
+
+    if [ "$install_type" == "Linuxpgp" ]; then
+        echo "您选择了 Linux多用户 环境下安装。"
+        installer_url="https://raw.githubusercontent.com/EAlyce/conf/main/PagerMaid/pgp.sh"
+    elif [ "$install_type" == "Linux" ]; then
+        echo "您选择了 Linux 环境下安装。"
+        installer_url="https://raw.githubusercontent.com/EAlyce/conf/main/PagerMaid/Pagermaid.sh"
+    elif [ "$install_type" == "Docker" ]; then
+        echo "您选择了 Docker 环境下安装。"
+        installer_url="https://raw.githubusercontent.com/EAlyce/conf/main/PagerMaid/DockerPagermaid.sh"
+    else
+        echo "错误的安装类型。"
+        exit 1
     fi
+
+    cd /var/lib || exit 1
+    sudo find /var/lib/ -type f -name "Pagermaid.sh*" -exec rm -f {} \;
+
+    curl -O "$installer_url" || {
+        echo "下载 Installer 失败"
+        exit 1
+    }
+
+    chmod +x "$(basename "$installer_url")" || {
+        echo "更改权限失败"
+        exit 1
+    }
+
+    "./$(basename "$installer_url")"
 }
 
+main_menu() {
+    while :
+    do
+        echo "----------------------------"
+        echo "      PagerMaid安装选项"
+        echo "----------------------------"
+        echo "[1] Linux多用户版本"
+        echo "[2] Linux(不推荐)"
+        echo "[3] Docker版本"
+        echo "[0] 退出"
+        echo "----------------------------"
+
+        read -p "输入选项 [0 - 3]: " choice
+
+        case "$choice" in
+            [0-3]) break;;
+            *) echo "输入无效，请重新输入";;
+        esac
+
+        case $choice in
+            1) 
+                install_pagermaid "Linuxpgp"
+                ;;
+            2)
+                install_pagermaid "Linux"
+                ;;
+            3)
+                install_pagermaid "Docker"
+                ;;
+            0) 
+                echo "退出"
+                exit
+                ;;
+            *)
+                echo "错误输入，请重新选择!"
+                ;;
+        esac
+
+        read -p "按任意键返回菜单 " 
+    done
+}
 set_custom_path
 kill_process
 remove_locks
 configure_packages
 update_dns
+main_menu
 
-while :
-do
-    echo "----------------------------"
-    echo "      PagerMaid安装选项"
-    echo "----------------------------"
-    echo "[1] Linux多用户版本"
-    echo "[2] Linux(不推荐)"
-    echo "[3] Docker版本"
-    echo "[0] 退出"
-    echo "----------------------------"
 
-    read -p "输入选项 [0 - 3]: " choice
 
-    case "$choice" in
-        [0-3]) break;;
-        *) echo "输入无效，请重新输入";;
-    esac
-
-    case $choice in
-        1) 
-            install_pagermaid "Linuxpgp"
-            ;;
-        2)
-            install_pagermaid "Linux"
-            ;;
-        3)
-            install_pagermaid "Docker"
-            ;;
-        
-        0) 
-            echo "退出"
-            exit
-            ;;
-       
-        *)
-            echo "错误输入，请重新选择!"
-            ;;
-    esac
-
-    read -p "按任意键返回菜单 " 
-done
