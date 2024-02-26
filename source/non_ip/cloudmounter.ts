8b40fc3dc01c320289de893aa4db347fdb8998ec
Alas <48439969+EAlyce@users.noreply.github.com>
2024-02-27 04:55:46 +0800

    Update

---
+++ source/non_ip/cloudmounter.ts
@@ -0,0 +1,10 @@
+export const DOMAINS = [
+  'DOMAIN-SUFFIX,sharepoint.com',
+  'DOMAIN-SUFFIX,graph.microsoft.com',
+  'DOMAIN,www.googleapis.com'
+];
+
+export const PROCESS_NAMES = [
+  'CloudMounter'
+  // 'RaiDrive'
+];
