816cb4aff0241df9acdbb07886ae6e3f8e221373
Alas <48439969+EAlyce@users.noreply.github.com>
2024-03-20 06:13:04 +0800

    Update

--- Linux/Linux.sh
+++ Linux/Linux.sh
@@ -5,14 +5,14 @@ function rename_nproc_conf() {
 }
 
 function update_common_session() {
-  if [ -f /etc/pam.d/common-session ] && [ -z "$(grep 'session required pam_limits.so' /etc/pam.d/common-session)" ]; then
-    echo "session required pam_limits.so" >> /etc/pam.d/common-session
+  local common_session="/etc/pam.d/common-session"
+  if [ -f "$common_session" ] && ! grep -q 'session required pam_limits.so' "$common_session"; then
+    echo "session required pam_limits.so" >> "$common_session"
   fi
 }
 
 function update_limits_conf() {
-  sed -i '/^# End of file/,$d' /etc/security/limits.conf
-  cat >> /etc/security/limits.conf <<EOF
+  cat <<EOF > /etc/security/limits.conf
 # End of file
 *     soft   nofile    1048576
 *     hard   nofile    1048576
@@ -22,23 +22,66 @@ function update_limits_conf() {
 *     hard   core      1048576
 *     hard   memlock   unlimited
 *     soft   memlock   unlimited
-root     soft   nofile    1048576
-root     hard   nofile    1048576
-root     soft   nproc     1048576
-root     hard   nproc     1048576
-root     soft   core      1048576
-root     hard   core      1048576
-root     hard   memlock   unlimited
-root     soft   memlock   unlimited
+root  soft   nofile    1048576
+root  hard   nofile    1048576
+root  soft   nproc     1048576
+root  hard   nproc     1048576
+root  soft   core      1048576
+root  hard   core      1048576
+root  hard   memlock   unlimited
+root  soft   memlock   unlimited
 EOF
 }
 
 function remove_unneeded_parameters() {
-  sed -i '/fs.file-max/d; /fs.inotify.max_user_instances/d; /net.core.somaxconn/d; /net.core.netdev_max_backlog/d; /net.core.rmem_max/d; /net.core.wmem_max/d; /net.ipv4.udp_rmem_min/d; /net.ipv4.udp_wmem_min/d; /net.ipv4.tcp_rmem/d; /net.ipv4.tcp_wmem/d; /net.ipv4.tcp_mem/d; /net.ipv4.udp_mem/d; /net.ipv4.tcp_syncookies/d; /net.ipv4.tcp_fin_timeout/d; /net.ipv4.tcp_tw_reuse/d; /net.ipv4.ip_local_port_range/d; /net.ipv4.tcp_max_syn_backlog/d; /net.ipv4.tcp_max_tw_buckets/d; /net.ipv4.route.gc_timeout/d; /net.ipv4.tcp_syn_retries/d; /net.ipv4.tcp_synack_retries/d; /net.ipv4.tcp_timestamps/d; /net.ipv4.tcp_max_orphans/d; /net.ipv4.tcp_no_metrics_save/d; /net.ipv4.tcp_ecn/d; /net.ipv4.tcp_frto/d; /net.ipv4.tcp_mtu_probing/d; /net.ipv4.tcp_rfc1337/d; /net.ipv4.tcp_sack/d; /net.ipv4.tcp_fack/d; /net.ipv4.tcp_window_scaling/d; /net.ipv4.tcp_adv_win_scale/d; /net.ipv4.tcp_moderate_rcvbuf/d; /net.ipv4.tcp_keepalive_time/d; /net.ipv4.tcp_notsent_lowat/d; /net.ipv4.conf.all.route_localnet/d; /net.ipv4.ip_forward/d; /net.ipv4.conf.all.forwarding/d; /net.ipv4.conf.default.forwarding/d; /net.core.default_qdisc/d; /net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+  local unneeded_parameters=(
+    fs.file-max
+    fs.inotify.max_user_instances
+    net.core.somaxconn
+    net.core.netdev_max_backlog
+    net.core.rmem_max
+    net.core.wmem_max
+    net.ipv4.udp_rmem_min
+    net.ipv4.udp_wmem_min
+    net.ipv4.tcp_rmem
+    net.ipv4.tcp_wmem
+    net.ipv4.tcp_mem
+    net.ipv4.udp_mem
+    net.ipv4.tcp_syncookies
+    net.ipv4.tcp_fin_timeout
+    net.ipv4.tcp_tw_reuse
+    net.ipv4.ip_local_port_range
+    net.ipv4.tcp_max_syn_backlog
+    net.ipv4.tcp_max_tw_buckets
+    net.ipv4.route.gc_timeout
+    net.ipv4.tcp_syn_retries
+    net.ipv4.tcp_synack_retries
+    net.ipv4.tcp_timestamps
+    net.ipv4.tcp_max_orphans
+    net.ipv4.tcp_no_metrics_save
+    net.ipv4.tcp_ecn
+    net.ipv4.tcp_frto
+    net.ipv4.tcp_mtu_probing
+    net.ipv4.tcp_rfc1337
+    net.ipv4.tcp_sack
+    net.ipv4.tcp_fack
+    net.ipv4.tcp_window_scaling
+    net.ipv4.tcp_adv_win_scale
+    net.ipv4.tcp_moderate_rcvbuf
+    net.ipv4.tcp_keepalive_time
+    net.ipv4.tcp_notsent_lowat
+    net.ipv4.conf.all.route_localnet
+    net.ipv4.ip_forward
+    net.ipv4.conf.all.forwarding
+    net.ipv4.conf.default.forwarding
+    net.core.default_qdisc
+    net.ipv4.tcp_congestion_control
+  )
+  sed -i '/^\('$(IFS='|'; echo "${unneeded_parameters[*]}")'\)\s*=/d' /etc/sysctl.conf
 }
 
 function add_new_parameters() {
-  cat >> /etc/sysctl.conf << EOF
+  cat <<EOF >> /etc/sysctl.conf
 fs.file-max = 1048576
 fs.inotify.max_user_instances = 8192
 net.core.somaxconn = 32768
@@ -81,7 +124,7 @@ net.ipv4.conf.default.forwarding = 0
 net.core.default_qdisc = fq_codel
 net.ipv4.tcp_congestion_control = bbr
 
-#SSR 对于会出现突发空闲的长周期 TLS 连接有很大影响，所以关闭
+# SSR 对于会出现突发空闲的长周期 TLS 连接有很大影响，所以关闭
 net.ipv4.tcp_slow_start_after_idle = 0
 
 #设置为1，当检测到 ICMP 黑洞时启用
