b1fd58aa8668055f3448e21ed91a873b2628a6f4
EAlyce <48439969+EAlyce@users.noreply.github.com>
2024-09-17 12:54:44 +0800

    Update Sub-Store_Docker-compose.sh

--- Sub-Store/Sub-Store_Docker-compose.sh
+++ Sub-Store/Sub-Store_Docker-compose.sh
@@ -10,56 +10,57 @@ check_root() {
 
 install_basic_tools() {
     # 更新包列表并安装基础工具
-    apt-get update -y
-    apt-get install -y curl gnupg lsb-release iptables net-tools netfilter-persistent software-properties-common
+    apt-get update -y > /dev/null 2>&1
+    apt-get install -y curl gnupg lsb-release iptables net-tools netfilter-persistent software-properties-common > /dev/null 2>&1
     echo "基础工具已安装。"
 }
 
 clean_system() {
     pkill -9 apt dpkg || true
     rm -f /var/lib/dpkg/lock* /var/lib/apt/lists/lock
-    dpkg --configure -a
-    apt-get clean autoclean
-    apt-get autoremove -y
+    dpkg --configure -a > /dev/null 2>&1
+    apt-get clean autoclean > /dev/null 2>&1
+    apt-get autoremove -y > /dev/null 2>&1
     rm -rf /tmp/*
     history -c && history -w
-    dpkg --list | awk '/^ii/{print $2}' | grep -E 'linux-(image|headers)-[0-9]' | grep -v "$(uname -r)" | xargs apt-get -y purge || true
+    dpkg --list | awk '/^ii/{print $2}' | grep -E 'linux-(image|headers)-[0-9]' | grep -v "$(uname -r)" | xargs apt-get -y purge > /dev/null 2>&1 || true
+    echo "系统清理已完成。"
 }
 
 install_packages() {
     export DEBIAN_FRONTEND=noninteractive
-    apt-get update -y
+    apt-get update -y > /dev/null 2>&1
     apt-get install -y \
         netcat-traditional apt-transport-https ca-certificates \
         iptables-persistent netfilter-persistent software-properties-common \
-        curl gnupg lsb-release
+        curl gnupg lsb-release > /dev/null 2>&1
 
     OS=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
     CODENAME=$(lsb_release -cs)
     ARCH=$(dpkg --print-architecture)
 
-    curl -fsSL https://download.docker.com/linux/${OS}/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    # 使用 -sSfL 保证静默输出，使用 -o 强制覆盖已存在的文件
+    curl -fsSL https://download.docker.com/linux/${OS}/gpg -o /usr/share/keyrings/docker-archive-keyring.gpg > /dev/null 2>&1
+
+    # 添加 Docker 源仓库
     echo "deb [arch=${ARCH} signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${OS} ${CODENAME} stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-    apt-get update -y
-    apt-get install -y docker-ce docker-ce-cli containerd.io
+    apt-get update -y > /dev/null 2>&1
+    apt-get install -y docker-ce docker-ce-cli containerd.io > /dev/null 2>&1
 
     if ! command -v docker-compose &> /dev/null; then
         LATEST_COMPOSE_VERSION=$(curl -sS https://api.github.com/repos/docker/compose/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
-        curl -sSL "https://github.com/docker/compose/releases/download/${LATEST_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        curl -sSL "https://github.com/docker/compose/releases/download/${LATEST_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose > /dev/null 2>&1
         chmod +x /usr/local/bin/docker-compose
     fi
 
-    apt-get clean
+    apt-get clean > /dev/null 2>&1
     rm -rf /var/lib/apt/lists/*
 
-    systemctl enable docker
-    systemctl start docker
+    systemctl enable docker > /dev/null 2>&1
+    systemctl start docker > /dev/null 2>&1
 
-    echo "Docker version:"
-    docker --version
-    echo "Docker Compose version:"
-    docker-compose --version
+    echo "Docker 和 Docker Compose 已成功安装。"
 }
 
 get_public_ip() {
@@ -77,17 +78,18 @@ get_public_ip() {
 }
 
 setup_environment() {
-    sudo locale-gen en_US.UTF-8 && sudo update-locale LANG=en_US.UTF-8 && sudo timedatectl set-timezone Asia/Shanghai
+    sudo locale-gen en_US.UTF-8 > /dev/null 2>&1 && sudo update-locale LANG=en_US.UTF-8 > /dev/null 2>&1 && sudo timedatectl set-timezone Asia/Shanghai > /dev/null 2>&1
     echo -e 'nameserver 8.8.4.4\nnameserver 8.8.8.8' > /etc/resolv.conf
     
     iptables -A INPUT -p udp --dport 60000:61000 -j ACCEPT
     iptables -A INPUT -p tcp --tcp-flags SYN SYN -j ACCEPT
     iptables-save > /etc/iptables/rules.v4
-    netfilter-persistent reload
+    netfilter-persistent reload > /dev/null 2>&1
     
     echo 0 > /proc/sys/net/ipv4/tcp_fastopen
-    docker system prune -af --volumes
+    docker system prune -af --volumes > /dev/null 2>&1
     echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf && sysctl -p > /dev/null
+    echo "环境设置已完成。"
 }
 
 setup_docker() {
@@ -108,29 +110,31 @@ services:
 EOF
 
     # 更新系统包并安装 cron
-    apt-get update -y && apt-get install -y cron
+    apt-get update -y > /dev/null 2>&1 && apt-get install -y cron > /dev/null 2>&1
 
     # 添加 cron 任务：每天凌晨5点更新 Docker 镜像并重启容器
     (crontab -l 2>/dev/null; echo "0 5 * * * docker-compose pull && docker-compose up -d || { echo 'Error: Unable to restart Docker containers' >&2; }") | crontab -
 
-    # 显示当前的 cron 任务以确认添加成功
-    crontab -l
-
     # 启动 Docker 容器并检查是否成功
-    docker-compose up -d || { echo "Error: Unable to start Docker containers" >&2; exit 1; }
-    echo "您的Sub-Store信息如下"
-    echo -e "\n后端地址：$public_ip:3001\n"
+    docker-compose up -d > /dev/null 2>&1 || { echo "错误：无法启动 Docker 容器。" >&2; exit 1; }
+    echo "Sub-Store 已成功设置。"
+    echo -e "\nSub-Store地址：$public_ip:3001\n"
     echo -e "\nAPI：http://$public_ip:3001/$secret_key\n"
 }
 
 main() {
     check_root
     install_basic_tools
+    echo "基础工具安装完成。"
     clean_system
+    echo "系统清理完成。"
     public_ip=$(get_public_ip)
     install_packages
+    echo "Docker 和 Docker Compose 安装完成。"
     setup_environment
+    echo "环境设置完成。"
     setup_docker
+    echo "Docker 环境已设置完毕。"
 }
 
 main
