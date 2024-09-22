ef452d502c5fcd341184178693e534a9c1a0d5a8
EAlyce <48439969+EAlyce@users.noreply.github.com>
2024-09-22 10:10:27 +0800

    Update install_snell.sh

--- Snell/install_snell.sh
+++ Snell/install_snell.sh
@@ -148,7 +148,7 @@ setup_docker() {
     cat <<EOF > docker-compose.yml
 services:
   snell:
-    image: accors/snell:latest
+    image: ghcr.io/skyxim/snell:latest
     container_name: Snell$PORT_NUMBER
     restart: always
     network_mode: host
