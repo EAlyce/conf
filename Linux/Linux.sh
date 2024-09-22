5f12e61f128195abf5f16e44da86b1590a4d98b7
EAlyce <48439969+EAlyce@users.noreply.github.com>
2024-09-22 11:56:00 +0800

    Update Linux.sh

--- Linux/Linux.sh
+++ Linux/Linux.sh
@@ -47,10 +47,10 @@ install_docker() {
     if ! command_exists docker || ! docker compose version &>/dev/null; then
         echo "安装Docker及Compose..."
         apt install -y apt-transport-https ca-certificates curl software-properties-common gnupg
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -o /dev/null
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
         apt update
-        apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin -o Dpkg::Options::="--force-overwrite"
+        apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
         systemctl enable --now docker
     fi
 }
