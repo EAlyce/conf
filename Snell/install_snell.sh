d559d85c224d96f2dae5e3024220d9f44f75ed27
EAlyce <48439969+EAlyce@users.noreply.github.com>
2024-09-23 23:02:25 +0800

    Update install_snell.sh

--- Snell/install_snell.sh
+++ Snell/install_snell.sh
@@ -1,17 +1,59 @@
 #!/bin/bash
 
-# 系统设置
-system_setup() {
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/EAlyce/conf/refs/heads/main/Linux/Linux.sh)"
+check_root() {
+    if [ "$(id -u)" != "0" ]; then
+        echo "运行脚本需要root权限"
+        exit 1
+    fi
+
+    if ! command -v apt &> /dev/null; then
+        echo "警告: 脚本仅支持 Debian 或 Ubuntu 系统。"
+        exit 1
+    fi
 }
 
-# 获取公网IP
+
+clean_lock_files() {
+
+    pkill -9 apt dpkg
+    rm -f /var/lib/dpkg/lock /var/lib/dpkg/lock-frontend /var/lib/apt/lists/lock
+    dpkg --configure -a > /dev/null
+    apt-get clean autoclean > /dev/null
+    apt-get autoremove -y > /dev/null
+    rm -rf /tmp/*
+    history -c && history -w
+    dpkg --list | awk '/^ii/{print $2}' | grep -E 'linux-(image|headers)-[0-9]' | grep -v "$(uname -r)" | xargs apt-get -y purge > /dev/null
+
+}
+
+
+install_tools() {
+    echo "开始更新系统并安装必要的软件包..."
+
+    # 更新系统并升级所有包
+    apt-get update -y || echo "更新失败"
+    # apt-get upgrade -y || echo "升级失败"
+    # apt-get dist-upgrade -y || echo "分发升级失败"
+    # apt-get full-upgrade -y || echo "完全升级失败"
+
+    # 安装所需的软件包
+    apt-get install -y \
+        netcat-traditional \
+        apt-transport-https \
+        ca-certificates \
+        iptables-persistent \
+        netfilter-persistent \
+        software-properties-common || echo "软件包安装失败"
+
+    echo "更新完成"
+}
+
 get_public_ip() {
     local ip_services=("ifconfig.me" "ipinfo.io/ip" "icanhazip.com" "ipecho.net/plain" "ident.me")
     for service in "${ip_services[@]}"; do
         public_ip=$(curl -s "$service" 2>/dev/null)
         if [[ "$public_ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-            echo "Public IP: $public_ip"
+            echo "Local IP: $public_ip"
             return
         fi
         sleep 1
@@ -20,7 +62,6 @@ get_public_ip() {
     exit 1
 }
 
-# 获取地理位置
 get_location() {
     local service="http://ipinfo.io/city"
     LOCATION=$(curl -s "$service" 2>/dev/null)
@@ -31,7 +72,24 @@ get_location() {
     fi
 }
 
-# 选择版本
+setup_environment() {
+    sudo locale-gen en_US.UTF-8 && sudo update-locale LANG=en_US.UTF-8 && sudo timedatectl set-timezone Asia/Shanghai
+    echo -e 'nameserver 8.8.4.4\nnameserver 8.8.8.8' > /etc/resolv.conf
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get update -qq
+    if ! command -v docker &> /dev/null; then
+        curl -fsSL https://get.docker.com | sh
+        systemctl enable --now docker
+    fi
+    iptables -A INPUT -p udp --dport 60000:61000 -j ACCEPT
+    iptables -A INPUT -p tcp --tcp-flags SYN SYN -j ACCEPT
+    iptables-save > /etc/iptables/rules.v4
+    netfilter-persistent reload
+    echo 0 > /proc/sys/net/ipv4/tcp_fastopen
+    docker system prune -af --volumes
+    echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf && sysctl -p > /dev/null
+}
+
 select_version() {
     echo "选择 Snell 版本："
     echo "1. Snell v3"
@@ -41,13 +99,12 @@ select_version() {
     choice="${choice:-2}"
     case $choice in
         0) echo "Exit script"; exit 0 ;;
-        1) BASE_URL="https://github.com/EAlyce/conf/raw/main/Snell/source"; SUB_PATH="v3.0.1/snell-server-v3.0.1"; VERSION_NUMBER="3" ;;
+        1) BASE_URL="https://github.com/EAlyce/conf/tree/main/Snell/source"; SUB_PATH="v3.0.1/snell-server-v3.0.1"; VERSION_NUMBER="3" ;;
         2) BASE_URL="https://dl.nssurge.com/snell"; SUB_PATH="snell-server-v4.1.0"; VERSION_NUMBER="4" ;;
         *) echo "Invalid selection"; exit 1 ;;
     esac
 }
 
-# 选择架构
 select_architecture() {
     ARCH="$(uname -m)"
     ARCH_TYPE="linux-amd64.zip"
@@ -55,13 +112,9 @@ select_architecture() {
     SNELL_URL="${BASE_URL}/${SUB_PATH}-${ARCH_TYPE}"
 }
 
-# 生成端口
 generate_port() {
     local ALLOWED_PORTS=(23456 23556)
-    if ! command -v nc.traditional &>/dev/null; then
-        apt-get install -y netcat-traditional
-    fi
-
+    command -v nc.traditional &> /dev/null || apt-get install -y netcat-traditional
     for PORT in "${ALLOWED_PORTS[@]}"; do
         if ! nc.traditional -z 127.0.0.1 "$PORT"; then
             PORT_NUMBER="$PORT"
@@ -78,36 +131,24 @@ generate_port() {
     done
 }
 
-# 设置防火墙
 setup_firewall() {
     local PORT="$1"
     iptables -A INPUT -p tcp --dport "$PORT" -j ACCEPT || { echo "Error: Unable to add firewall rule"; exit 1; }
 }
 
-# 生成密码
 generate_password() {
     PASSWORD=$(openssl rand -base64 18) || { echo "Error: Unable to generate password"; exit 1; }
+
 }
 
-# 设置 Docker
 setup_docker() {
-    if ! command -v docker &>/dev/null; then
-        echo "Docker is not installed, please install Docker first."
-        exit 1
-    fi
-
-    if ! command -v docker-compose &>/dev/null; then
-        echo "Docker Compose is not installed, please install Docker Compose first."
-        exit 1
-    fi
-
     local NODE_DIR="/root/snelldocker/Snell$PORT_NUMBER"
     mkdir -p "$NODE_DIR/snell-conf" || { echo "Error: Unable to create directory $NODE_DIR"; exit 1; }
     cd "$NODE_DIR" || { echo "Error: Unable to change directory to $NODE_DIR"; exit 1; }
     cat <<EOF > docker-compose.yml
 services:
   snell:
-    image: accors/snell:latest
+    image: ghcr.io/skyxim/snell:latest
     container_name: Snell$PORT_NUMBER
     restart: always
     network_mode: host
@@ -130,16 +171,21 @@ EOF
     echo "节点信息如下"
 }
 
-# 打印节点信息
 print_node() {
     echo -e "\n\n\n$LOCATION $PORT_NUMBER = snell, $public_ip, $PORT_NUMBER, psk=$PASSWORD, version=$VERSION_NUMBER\n\n\n"
 }
 
-# 主函数
+
 main() {
-    system_setup
+    check_root
+    clean_lock_files &
+    CLEAN_LOCK_FILES_PID=$!
+    # 等待 clean_lock_files 完成
+    wait $CLEAN_LOCK_FILES_PID
     get_public_ip
     get_location
+    install_tools
+    setup_environment
     select_version
     select_architecture
     generate_port
