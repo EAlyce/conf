f8a8fff20e8010b850f6cb87b33d96ed0b32777b
EAlyce <48439969+EAlyce@users.noreply.github.com>
2024-10-15 13:42:10 +0800

    Update hysteria2.sh

--- hysteria2/hysteria2.sh
+++ hysteria2/hysteria2.sh
@@ -44,24 +44,15 @@ get_public_ip() {
 setup_environment() {
     sudo locale-gen en_US.UTF-8
     sudo update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
-
-    echo -e "nameserver 8.8.4.4\nnameserver 8.8.8.8" > /etc/resolv.conf
     { echo -e "net.ipv4.tcp_fastopen = 0\nnet.core.default_qdisc = fq\nnet.ipv4.tcp_congestion_control = bbr\nnet.ipv4.tcp_ecn = 1\nvm.swappiness = 0" >> /etc/sysctl.conf && sysctl -p; } > /dev/null 2>&1 && echo "设置已完成"
     iptables -A INPUT -p udp --dport 60000:61000 -j ACCEPT > /dev/null || true
     for iface in $(ls /sys/class/net | grep -v lo); do
         ip link set dev "$iface" mtu 1500
     done
 }
+$RANDOM_PORT
+设置端口范围
 
-generate_port() {
-    while true; do
-        RANDOM_PORT=$(shuf -i 5000-30000 -n 1)
-        if ! nc.traditional -z 127.0.0.1 "$RANDOM_PORT"; then
-            echo "Selected random port: $RANDOM_PORT"
-            break
-        fi
-    done
-}
 
 setup_firewall() {
     iptables -A INPUT -p tcp --dport "$RANDOM_PORT" -j ACCEPT || { echo "Error: Unable to add firewall rule"; exit 1; }
