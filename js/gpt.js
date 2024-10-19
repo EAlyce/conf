0f18a3883b559aa88ce95191153c66fdff8dafb3
EAlyce <48439969+EAlyce@users.noreply.github.com>
2024-10-20 00:48:58 +0800

    Update gpt.js

--- js/gpt.js
+++ js/gpt.js
@@ -1,48 +1,47 @@
 let url = "http://chat.openai.com/cdn-cgi/trace";
+let tf = ["T1","XX","AL","DZ","AD","AO","AG","AR","AM","AU","AT","AZ","BS","BD","BB","BE","BZ","BJ","BT","BA","BW","BR","BG","BF","CV","CA","CL","CO","KM","CR","HR","CY","DK","DJ","DM","DO","EC","SV","EE","FJ","FI","FR","GA","GM","GE","DE","GH","GR","GD","GT","GN","GW","GY","HT","HN","HU","IS","IN","ID","IQ","IE","IL","IT","JM","JP","JO","KZ","KE","KI","KW","KG","LV","LB","LS","LR","LI","LT","LU","MG","MW","MY","MV","ML","MT","MH","MR","MU","MX","MC","MN","ME","MA","MZ","MM","NA","NR","NP","NL","NZ","NI","NE","NG","MK","NO","OM","PK","PW","PA","PG","PE","PH","PL","PT","QA","RO","RW","KN","LC","VC","WS","SM","ST","SN","RS","SC","SL","SG","SK","SI","SB","ZA","ES","LK","SR","SE","CH","TH","TG","TO","TT","TN","TR","TV","UG","AE","US","UY","VU","ZM","BO","BN","CG","CZ","VA","FM","MD","PS","KR","TW","TZ","TL","GB"];
 
-let titlediy, icon, iconerr, iconColor, iconerrColor;
+// 处理 argument 参数
+let titlediy;
 if (typeof $argument !== 'undefined') {
-    const args = $argument.split('&');
-    for (let i = 0; i < args.length; i++) {
-        const [key, value] = args[i].split('=');
-        if (key === 'title') {
-            titlediy = value;
-        } else if (key === 'icon') {
-            icon = value;
-        } else if (key === 'iconerr') {
-            iconerr = value;
-        } else if (key === 'icon-color') {
-            iconColor = value;
-        } else if (key === 'iconerr-color') {
-            iconerrColor = value;
-        }
+  const args = $argument.split('&');
+  for (let i = 0; i < args.length; i++) {
+    const [key, value] = args[i].split('=');
+    if (key === 'title') {
+      titlediy = value;
     }
+  }
 }
 
-$httpClient.get(url, function (error, response, data) {
-    if (error) {
-        console.error(error);
-        $done();
-        return;
-    }
+$httpClient.get(url, function(error, response, data) {
+  if (error) {
+    console.error(error);
+    $done();
+    return;
+  }
 
-    let gpt, iconUsed, iconCol;
-    if (data.includes("gpt")) {  // 直接检测数据中是否包含 'gpt'
-        gpt = "GPT: 支持";
-        iconUsed = icon ? icon : undefined;
-        iconCol = iconColor ? iconColor : undefined;
-    } else {
-        gpt = "GPT: 不支持";
-        iconUsed = iconerr ? iconerr : undefined;
-        iconCol = iconerrColor ? iconerrColor : undefined;
-    }
+  let lines = data.split("\n");
+  let cf = lines.reduce((acc, line) => {
+    let [key, value] = line.split("=");
+    acc[key] = value;
+    return acc;
+  }, {});
+  
+  let loc = cf.loc;
 
-    let body = {
-        title: titlediy || "默认标题",
-        content: gpt,
-        icon: iconUsed,
-        'icon-color': iconCol
-    };
+  let l = tf.indexOf(cf.loc);
+  let gpt;
+  
+  if (l !== -1) {
+    gpt = "GPT: 解锁";
+  } else {
+    gpt = "GPT: 未解锁";
+  }
 
-    $done(body);
+  let body = {
+    title: titlediy ? titlediy : 'ChatGPT',
+    content: `${gpt}   区域: ${loc}`
+  };
+
+  $done(body);
 });
