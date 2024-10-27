68c6e9000f5d1992ab09c5e1f45b3d7cc7713674
EAlyce <48439969+EAlyce@users.noreply.github.com>
2024-10-28 04:53:00 +0800

    更新 ip-api.js

--- js/ip-api.js
+++ js/ip-api.js
@@ -5,11 +5,18 @@ if ($response.statusCode != 200) {
 var body = $response.body;
 var obj = JSON.parse(body);
 
+// 提取ASN号
+var asField = obj['as'];
+var asn = asField.split(' ')[0]; // 提取 'AS400618' 这样的ASN号
+
 var title = obj['country'];
 var subtitle = obj['as'] + ' ' + obj['isp'];
 var ip = obj['query'];
 
-var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['as'];
-
+var description = "国家" + ":" + obj['country'] + '\n' + 
+                  "城市" + ":" + obj['city'] + '\n' + 
+                  "运营商" + ":" + obj['isp'] + '\n' + 
+                  "数据中心" + ":" + obj['as'] + '\n' + 
+                  "ASN" + ":" + asn;
 
 $done({title, subtitle, ip, description});
