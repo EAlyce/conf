8c1c9e92fe66ad73c41f20b602bcdac0ca33bfb7
EAlyce <48439969+EAlyce@users.noreply.github.com>
2024-10-28 03:07:04 +0800

    更新 ip-api.js

--- js/ip-api.js
+++ js/ip-api.js
@@ -6,10 +6,10 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 // Extract relevant fields
-var title = obj['country'];
-var subtitle = obj['query'] + ' ' + obj['country'];  // Only query (IP) and country
-var ip = obj['query'];
-var description = '------------------------------' + '\n' + '\n' + '服务商:' + obj['isp'] + '\n' + '\n' + '地区:' + obj['regionName'] + '\n' + '\n' + 'IP地址:' + obj['query'] + '\n';
+var title = obj['country'];  // Country code (e.g., JP)
+var subtitle = obj['ip'] + ' ' + obj['country'];  // IP and country code only
+var ip = obj['ip'];
+var description = '------------------------------' + '\n' + '\n' + '服务商:' + obj['org'] + '\n' + '\n' + '地区:' + obj['region'] + '\n' + '\n' + 'IP地址:' + obj['ip'] + '\n';
 
 // Finish the request
 $done({ title, subtitle, ip, description });
