94a7534a6d5f46288b53fba7ac3bb8589b074b9a
EAlyce <48439969+EAlyce@users.noreply.github.com>
2024-10-28 03:20:39 +0800

    更新 ip-api.js

--- js/ip-api.js
+++ js/ip-api.js
@@ -2,17 +2,15 @@ if ($response.statusCode != 200) {
   $done(null);
 }
 
-var body = $response.body;
-var obj = JSON.parse(body);
 
-// Extract relevant fields based on the ipinfo.io structure
-var title = obj['country'];  // Country code (e.g., JP for Japan)
-var subtitle = obj['ip'] + ' ' + obj['country'];  // Only IP and country
-var ip = obj['ip'];
+var title = obj['country']; 
+var subtitle = obj['org'];  
+var ip = obj['loc'];
+
 var description = '------------------------------' + '\n\n' + 
                   '服务商: ' + obj['org'] + '\n\n' + 
                   '地区: ' + obj['region'] + '\n\n' + 
-                  'IP地址: ' + obj['ip'] + '\n';
+                  'IP地址: ' + obj['loc'] + '\n';
 
 // Complete the response
 $done({ title, subtitle, ip, description });
