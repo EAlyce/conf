19c55993c9b97a9a6debb7fee1599607ba3d2de0
EAlyce <48439969+EAlyce@users.noreply.github.com>
2024-10-28 05:27:07 +0800

    更新 ip-api.js

--- js/ip-api.js
+++ js/ip-api.js
@@ -1,32 +1,46 @@
-if ($response.statusCode !== 200) {
-  console.error("请求失败，状态码：" + $response.statusCode);
+if ($response.statusCode != 200) {
   $done(null);
+}
+
+var city0 = "高谭市";
+var isp0 = "Cross-GFW.org";
+
+function City_ValidCheck(para) {
+  return para || city0;
+}
+
+function ISP_ValidCheck(para) {
+  return para || isp0;
+}
+
+function Country_ValidCheck(para) {
+  return para || "未知国家";
 }
 
 var body = $response.body;
-console.log("响应体：" + body); // 调试输出响应体
-var obj;
+var obj = JSON.parse(body);
 
-try {
-  obj = JSON.parse(body);
-} catch (e) {
-  console.error("JSON 解析错误：" + e);
-  $done(null);
-}
+// 获取国家信息
+var title = Country_ValidCheck(obj["country"]);
 
-// 检查解析后的对象
-console.log("解析后的对象：" + JSON.stringify(obj)); // 调试输出解析后的对象
+// subtitle 显示ISP和ASN信息
+var subtitle = ISP_ValidCheck(obj["org"] || obj.as) + " | ASN: " + (obj["as"] || "未知ASN");
 
-var title = obj['country'] || '未知国家'; // 若没有国家代码则使用默认值
-var subtitle = obj['org'] || '未知运营商'; // 若没有组织则使用默认值
-var ip = obj['ip'] || '未知IP'; // 若没有IP则使用默认值
+// IP信息
+var ip = obj["query"];
 
-var description = "国家代码" + ":" + (obj['country'] || '未知') + '\n' + 
-                  "城市" + ":" + (obj['city'] || '未知') + '\n' + 
-                  "地区" + ":" + (obj['region'] || '未知') + '\n' + 
-                  "运营商" + ":" + (obj['org'] || '未知') + '\n' + 
-                  "邮政编码" + ":" + (obj['postal'] || '未知') + '\n' + 
-                  "坐标" + ":" + (obj['loc'] || '未知') + '\n' + 
-                  "时区" + ":" + (obj['timezone'] || '未知');
+// 详细描述
+var description =
+  "服务商:" +
+  obj["isp"] +
+  "\n" +
+  "地区:" +
+  City_ValidCheck(obj["regionName"]) +
+  "\n" +
+  "IP:" +
+  obj["query"] +
+  "\n" +
+  "时区:" +
+  obj["timezone"];
 
 $done({ title, subtitle, ip, description });
