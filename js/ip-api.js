6398413d32bb7afb32254532b31ca4bee35f2eab
EAlyce <48439969+EAlyce@users.noreply.github.com>
2024-10-28 03:39:57 +0800

    更新 ip-api.js

--- js/ip-api.js
+++ js/ip-api.js
@@ -5,15 +5,10 @@ if ($response.statusCode != 200) {
 var body = $response.body;
 var obj = JSON.parse(body);
 
-// 正则表达式提取AS信息
-var asInfo = obj['org'].match(/AS\d+/) ? obj['org'].match(/AS\d+/)[0] : 'AS信息不可用';
-
 var title = obj['country'];
-// 在 subtitle 中加入 org, AS 信息, IP 地址
-var subtitle = obj['org'] + ' ' + asInfo + ' ' + obj['ip'];
 
-var ip = obj['ip'];
+var subtitle = obj['as'] + ' ' + obj['query'];
+var ip = obj['query'];
 
-var description = '------------------------------'+'\n'+'\n'+'服务商:'+obj['org'] + '\n'+'\n'+'AS 信息:' + asInfo + '\n' + '地区:' +obj['region']+ '\n'+ '\n' + 'IP地址:'+ obj['ip'] + '\n';
-
+var description = '------------------------------'+'\n'+'\n'+'服务商:'+obj['isp'] + '\n'+'\n'+'地区:' +obj['regionName']+ '\n'+ '\n' + 'IP地址:'+ obj['query'] + '\n';
 $done({title, subtitle, ip, description});
