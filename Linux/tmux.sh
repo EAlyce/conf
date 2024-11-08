11865abeb6437161f52dc323dfe05f0435a790d4
EAlyce <48439969+EAlyce@users.noreply.github.com>
2024-11-08 19:09:21 +0800

    Update

--- Linux/tmux.sh
+++ Linux/tmux.sh
@@ -7,21 +7,29 @@ then
     sudo apt-get install -y tmux
 fi
 
+
 echo "当前 tmux 会话:"
 tmux list-sessions | cat -n
 
-read -p "请输入序号进入会话: " session_num
-session_name=$(tmux list-sessions | sed -n "${session_num}p" | cut -d':' -f1)
-tmux attach-session -t "$session_name"
-
-if [ -z "$session_name" ]
+read -p "请输入序号进入会话(或输入 'n' 创建新会话, 'd' 删除会话): " session_input
+if [ "$session_input" = "n" ]
 then
-    read -p "当前没有 tmux 会话,是否创建一个新会话?(y/n) " create_new
-    if [ "$create_new" = "y" ]
-    then
-        session_name="tmux_$(openssl rand -hex 4)"
-        tmux new-session -s "$session_name" -d
-        echo "已创建新会话: $session_name"
-        tmux attach-session -t "$session_name"
-    fi
+
+    read -p "请输入新会话名称: " new_session_name
+    tmux new-session -s "$new_session_name" -d
+    echo "已创建新会话: $new_session_name"
+    tmux attach-session -t "$new_session_name"
+elif [ "$session_input" = "d" ]
+then
+
+    echo "当前 tmux 会话:"
+    tmux list-sessions | cat -n
+    read -p "请输入要删除的会话序号: " del_session_num
+    del_session_name=$(tmux list-sessions | sed -n "${del_session_num}p" | cut -d':' -f1)
+    tmux kill-session -t "$del_session_name"
+    echo "已删除会话: $del_session_name"
+else
+
+    session_name=$(tmux list-sessions | sed -n "${session_input}p" | cut -d':' -f1)
+    tmux attach-session -t "$session_name"
 fi
