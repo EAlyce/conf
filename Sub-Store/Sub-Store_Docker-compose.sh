e60514d450c0fa47d11d365b3633f2965a70a519
EAlyce <48439969+EAlyce@users.noreply.github.com>
2025-07-09 03:55:28 +0000

    2025-07-09 11:55:28

--- Sub-Store/Sub-Store_Docker-compose.sh
+++ Sub-Store/Sub-Store_Docker-compose.sh
@@ -28,7 +28,6 @@ install_packages() {
 get_public_ip() {
     local ip_services=("ifconfig.me" "ipinfo.io/ip" "icanhazip.com" "ipecho.net/plain" "ident.me")
     local public_ip
-    
     for service in "${ip_services[@]}"; do
         if public_ip=$(curl -sS --connect-timeout 5 "$service"); then
             if [[ "$public_ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
@@ -38,26 +37,18 @@ get_public_ip() {
         fi
         sleep 1
     done
-    
     echo "无法获取公共 IP 地址。" >&2
     exit 1
 }
 
 setup_docker() {
-    # 生成随机密钥
     local secret_key
     secret_key=$(openssl rand -hex 16)
     echo "生成的密钥: $secret_key"
-    
-    # 创建数据目录
     mkdir -p /root/sub-store-data
-    
-    # 清理旧容器和配置
     echo "清理旧容器和配置..."
     docker rm -f sub-store >/dev/null 2>&1 || true
     docker compose -p sub-store down >/dev/null 2>&1 || true
-    
-    # 创建 docker-compose.yml 文件
     cat <<EOF > docker-compose.yml
 name: sub-store-app
 services:
@@ -73,29 +64,33 @@ services:
     volumes:
       - /root/sub-store-data:/opt/app/data
 EOF
-    
-    # 拉取最新镜像并启动容器
     echo "拉取最新镜像..."
     docker compose -p sub-store pull
-    
     echo "启动容器..."
     docker compose -p sub-store up -d
-    
-    # 安装和配置 cron
+
+    echo "正在清理未使用的镜像和缓存..."
+    unused_images=$(docker images -f "dangling=true" -q)
+    if [ -n "$unused_images" ]; then
+        echo "以下镜像将被删除："
+        docker images -f "dangling=true"
+        docker image prune -f
+    else
+        echo "没有未使用的镜像需要清理。"
+    fi
+
     if ! command -v cron &>/dev/null; then
         echo "安装 cron..."
         apt-get update >/dev/null 2>&1
         apt-get install -y cron >/dev/null 2>&1
     fi
-    
+
     systemctl enable cron >/dev/null 2>&1
     systemctl start cron
-    
-    # 更新 cron 任务
-    local cron_job="0 * * * * cd $(pwd) && docker stop sub-store && docker rm sub-store && docker compose pull sub-store && docker compose up -d sub-store >/dev/null 2>&1"
+
+    local cron_job="0 * * * * cd $(pwd) && docker stop sub-store && docker rm sub-store && docker compose pull sub-store && docker compose up -d sub-store && docker image prune -f >/dev/null 2>&1"
     (crontab -l 2>/dev/null || true; echo "$cron_job") | sort -u | crontab -
-    
-    # 等待容器完全启动
+
     echo "等待服务启动..."
     for i in {1..30}; do
         if curl -s "http://127.0.0.1:3001" >/dev/null; then
@@ -106,15 +101,15 @@ EOF
         fi
         sleep 1
     done
-    
+
     echo "警告: 服务似乎未能在预期时间内启动，但可能仍在进行中。"
-    echo 
+    echo
     echo -e "\n您的 Sub-Store 信息如下："
-    echo 
+    echo
     echo -e "\nSub-Store 面板：http://$public_ip:3001"
-    echo 
+    echo
     echo -e "后端地址：http://$public_ip:3001/$secret_key\n"
-    echo 
+    echo
 }
 
 main() {
